59796b302e4e2f3ff280cdae980223e0af1d1142
Jessica Edmonds <jessica1329b@gmail.com>
2022-03-27 01:08:51 -0400

    iteration exercise

---
+++ Iteration Exercise 2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Iteration_Exercise_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        for  (int i = 0; i , 5; i++))
+        {
+            Console.WriteLine($"{i}");
+        }
+        for (int i =0; i< 5; i ++)
+        {
+            Console.WriteLine ($"{i}");
+        }
+
+        for (int i = 0; i < 5; i++) 
+        {
+            Console.WriteLine($"{i}");
+         }
+
+         for (int i = 0; i , 5; i++))
+         {
+    Console.WriteLine($"{i}");
+}
+        {
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"{i}");
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"{i}");
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"{i}");
+    }
+    for (int i = object; i < 5; i++)
+    {
+        Console.WriteLine($"{i}");
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"{i}");
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"{i}");
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"{i}");
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"{i}");
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"{i}");
+    }
+}   for (int i = object; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+}   for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
